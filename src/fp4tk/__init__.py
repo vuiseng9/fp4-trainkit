fc5ee68a74af84b7e74c25fc020a1a2e92e8e71a
Vui Seng Chua <vuiseng9@gmail.com>
2025-07-28 00:26:20 -0700

    Revise packaging

---
+++ src/fp4tk/__init__.py
@@ -0,0 +1,7 @@
+from .utils import FP4LinearConverter
+from .recipe import FP4_RECIPES
+
+__all__ = [
+    "FP4LinearConverter",
+    "FP4_RECIPES",
+]
--- src/fp4tk/utils.py
+++ src/fp4tk/utils.py
@@ -5,7 +5,7 @@ from fp4tk.linear import FP4Linear
 
 class FP4LinearConverter:
     @staticmethod
-    def apply(model: nn.Module, recipe: RecipeConfig, keywords: list[str] = None, verbose: bool = True):
+    def apply(model: nn.Module, recipe: RecipeConfig, keywords: list[str] = None, verbose: bool = False):
         def replace_linear_with_FP4Linear(model, recipe, keywords):
             """Recursively replace nn.Linear with FP4Linear in the model."""
             for name, module in model.named_children():
